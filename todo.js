9da3a6a234892f9e9a82d303805c9022a4fd27bd
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-10-25 19:56:22 +0700

    Release 0-2

--- todo.js
+++ todo.js
@@ -1,1 +1,21 @@
+const Controller = require('./controller')
+
+class Todo{
+	static readInput(input){
+		Controller.processInput(input)
+	}
+
+	static argv(){
+		let input = []
+		process.argv.forEach((val, index) => {
+  			if(index>1){
+  				input.push(val)
+  			}
+		});
+
+		this.readInput(input)		
+	}
+}
+
+Todo.argv()
 
