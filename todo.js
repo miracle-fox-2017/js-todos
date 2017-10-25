146597ec8efc86b23b6ce9324e425d6375a9d3c4
angrha <angrha@gmail.com>
2017-10-25 22:19:50 +0700

    first commit, masih bingung lemparan lepmarannya.aang.

--- todo.js
+++ todo.js
@@ -1,1 +1,26 @@
+//ini controller
 
+const dataToDo = require('./data')
+const show = require('./view')
+
+class ToDo{
+  constructor(input){
+    this.input = input
+  }
+  command(){
+    if(this.input == 'help'){
+      show.help()
+    }
+    else if(this.input == 'list'){
+      show.list(data.bacaFile())
+    }
+
+  }
+}
+
+let data = new dataToDo('data.json')
+let whatToDo = data.bacaFile()
+
+let argv = process.argv.slice(2)
+let todo = new ToDo(argv)
+let userInput = todo.command()
